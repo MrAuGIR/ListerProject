0354d22154d1aaf79f547dce572b8f1eba55dac8
MrAuGIR <augirard17@gmail.com>
2020-10-23 15:18:44 +0200

    mise a jour 15h30

--- scripts/switch.js
+++ scripts/switch.js
@@ -3,63 +3,33 @@ var index;
 
 var tabid = document.getElementsByClassName("center_illustration");
 
-
-
-//var tabid = ['illustration_default', 'illustration_second', 'illustration_third'];
-
+/* au chargement de la page  on 'des-affiche' les bloc de class 'center_illustration' sauf le premier */
 window.onload = function() {
     tabid[0].style.display ="flex";
     for (i=1; i<tabid.length;i++)
     {
-        tabid.style.display ="none";
+        tabid[i].style.display ="none";
     }
-   
     index=0;
 }
 
-
-
+/* au clic sur une des deux flèche, décalage de l'index du tabid à gauche ou a droite, 
+disparition de l'ancien bloc, affichage du nouveau */
 function switchElement(direction)
 {
-    var i, tabElmContent, arrowLeft, arrowRight;
-
-    document.getElementById(tabid[index]).style.display = "none";
+    
+    tabid[index].style.display = "none";
 
     var idElement = document.getElementById(direction).id;
     if(idElement == 'clickLeft')
     {
-        
-        if(index==0){
-            index=tabid.length;
-        }
-        else{
-            index-=1;
-        }
+        index = (index===0) ? (tabid.length-1) : index-=1;
     }
     else
     {
-        if(index==tabid.length)
-        {
-            index=0;
-        }
-        else
-        {
-            index+=1;
-        }
+        index = (index===tabid.length-1) ? 0 : index+=1;
     }
-    var textId = tabid[index];
-    var element = document.getElementById(textId);
-    element.style.display = "flex";
-    /*
-    tabElmContent = document.getElementsByClassName("center_illustration");
-    for(i = 0; i<tabElmContent.length; i++)
-    {
-        tabElmContent[i].style.display ="none";
-    }
-    
-    */
-    
-    
+    tabid[index].style.display = "flex";
 
 }
 
