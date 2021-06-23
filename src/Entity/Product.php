d2e23590823149aa9404c08885f153ea759fe809
MrAuGir <augirard17@gmail.com>
2021-06-23 15:33:34 +0200

    add relation between category and product

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -29,6 +29,11 @@ class Product
      */
     private $listeLines;
 
+    /**
+     * @ORM\ManyToOne(targetEntity=Category::class, inversedBy="products")
+     */
+    private $category;
+
     public function __construct()
     {
         $this->listeLines = new ArrayCollection();
@@ -77,6 +82,18 @@ class Product
                 $listeLine->setProduct(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
 
         return $this;
     }
