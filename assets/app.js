530eb5b187236cfdd14e4c1400776db8963ab7ca
MrAuGir <augirard17@gmail.com>
2021-07-16 18:58:02 +0200

    init react and librairies

--- assets/app.js
+++ assets/app.js
@@ -4,9 +4,12 @@
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
+//import de react
+import React from 'react';
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
-// start the Stimulus application
-import './bootstrap';
+const App = () => {
+    return <h1>Bonjour (test jsx) </h1>;
+}
--- readme.md
+++ readme.md
@@ -7,10 +7,11 @@
 1. Symfony 5.3
 2. Api Platform 2.6
 3. MySQL 8.0.2
-4. React 
-5. WebPack encore
-6. Bootstrap 5
-7. php 8.0.3
-8. JWT token
-9. openssl
+4. React 16.8.6
+5. Axios 0.18
+6. WebPack encore
+7. Bootstrap 5
+8. php 8.0.3
+9. JWT token
+10. openssl
    
