a5a17fa38110aac631abd085358f6468b7f629cb
MrAuGir <augirard17@gmail.com>
2021-07-17 11:08:38 +0200

    script token login

--- assets/app.js
+++ assets/app.js
@@ -4,12 +4,15 @@
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
-//import de react
-import React from 'react';
-
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
+//import de react
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+
+
+
 const App = () => {
-    return <h1>Bonjour (test jsx) </h1>;
+    
 }
---
+++ assets/js/services/LoginAPI.js
@@ -0,0 +1,76 @@
+import Axios from 'axios';
+import jwtDecode from "jwt-decode";
+import { LOGIN_API } from "../config";
+
+/**
+ * LOGOUT (Delete token)
+ */
+function logout(){
+    window.localStorage.removeItem("loginToken");
+    delete Axios.defaults.headers['Authorization'];
+}
+
+/**
+ * Http request login
+ * @param {object} credentials
+ * @return
+ */
+function login(credentials){
+
+    return Axios.post(LOGIN_API, credentials)
+        .then(response => response.data.token)
+        .then(token => {
+            window.localStorage.setItem("loginToken", token);
+            setAxiosToken(token);
+        })
+}
+
+/**
+ * Set jwt token bearer on Axios default header
+ */
+function setAxiosToken(token){
+    Axios.defaults.headers['Authorization'] = "Bearer "+token;
+}
+
+/**
+ * Get saved token at the application's start
+ */
+function setup(){
+
+    //check if token exist
+    const token = window.localStorage.getItem('loginToken');
+
+    if(token){
+
+        //j'extirpe la donné exp de data renvoyé par jwtDecode(token)
+        const {exp} = jwtDecode(token);
+        if( exp * 1000 > new Date().getItem() ){
+            setAxiosToken(token);
+        }
+    }
+}
+
+/**
+ * Check if user is login and authenticade
+ */
+function isLogin(){
+
+    const token = window.localStorage.getItem('loginToken');
+
+    if(token){
+        const {exp} = jwtDecode(token);
+        // *1000 for having the two datetime in ms
+        if( exp*1000 > new Date().getTime() ){
+            return true;
+        }
+        return false;
+    }
+    return false;
+}
+
+export default{
+    login,
+    logout,
+    setup,
+    isLogin
+};
