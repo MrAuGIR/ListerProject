abf16186ced025a01a38c8eecb27f77ddfd85085
MrAuGir <augirard17@gmail.com>
2021-07-17 11:08:24 +0200

    config var api

---
+++ assets/js/config.js
@@ -0,0 +1,7 @@
+export const API_URL = process.env.API_URL;
+
+export const LISTE_API = API_URL + "listes";
+export const PRODUCT_API = API_URL + "products";
+export const CATEGORY_API = API_URL + "categories";
+export const USER_API = API_URL + "users";
+export const LISTE_LINE_API = API_URL + "liste_lines";
