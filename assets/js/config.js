525c2facdbbd1a9442f1e9a8e55c203fbd3de573
MrAuGir <augirard17@gmail.com>
2021-07-18 11:41:34 +0200

    variables de configuration

--- assets/js/config.js
+++ assets/js/config.js
@@ -3,6 +3,6 @@ export const API_URL = process.env.API_URL;
 export const LISTE_API = API_URL + "listes";
 export const PRODUCT_API = API_URL + "products";
 export const CATEGORY_API = API_URL + "categories";
-export const USER_API = API_URL + "users";
 export const LISTE_LINE_API = API_URL + "liste_lines";
+export const USER_API = API_URL + "users";
 export const LOGIN_API = API_URL + "login_check";
