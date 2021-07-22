89aaaf302b6af28098112c42ea078dfcaedf5f25
MrAuGir <augirard17@gmail.com>
2021-07-22 15:54:49 +0200

    cache and service listes API

---
+++ assets/js/services/ListesAPI.js
@@ -0,0 +1,50 @@
+import Axios from 'axios';
+import {LISTE_API} from '../config';
+import Cache from './Cache';
+
+async function findAll(){
+    //check cache
+    const cachedListes = await Cache.get('listes');
+
+    if(cachedListes) return cachedListes;
+
+    return Axios.get(LISTE_API)
+            .then(response => {
+                const listes = response.data['hydra:member'];
+                Cache.set('listes', listes);
+                return listes;
+            });
+}
+
+
+async function find(id){
+    const cachedListes = await Cache.get('listes.' + id);
+    if(cachedListes) return cachedListes;
+
+    return Axios.get(LISTE_API + '/' + id)
+            .then( response => {
+                const liste = response.data;
+                Cache.set('listes.'+id, liste);
+                return liste;
+            })
+}
+
+async function create(){
+    return Axios.post(LISTE_API, liste)
+            .then( async response => {
+                const cachedListes = await Cache.get('listes');
+                if(cachedListes){
+                    Cache.set('listes', [...cachedListes, response.data]);
+                }
+                return response;
+            })
+}
+
+
+
+
+export default{
+    findAll:findAll,
+    find:find,
+    create:create
+}
